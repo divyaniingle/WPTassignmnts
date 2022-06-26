7be6fe1306a24be60fa5cee0f8e869c04c2d8cc1
divyaniingle <101325204+divyaniingle@users.noreply.github.com>
2022-06-27 02:05:10 +0530

    Add files via upload

---
+++ wptAssignment/assign2_1.js
@@ -0,0 +1,107 @@
+let dbparams =
+{
+    host: 'localhost',
+    user: 'root',
+    password: 'cdac',
+    database: 'Amravati',
+    port: 3306
+};
+const mysql = require('mysql2'); 
+const con = mysql.createConnection(dbparams);
+
+const express = require('express');
+const app = express();
+
+app.use(express.static("static"));
+
+
+app.get("/getItem", (req, resp) => {
+    let input = req.query.x;
+    console.log(input);
+    let output = { itemnofoundstatus: false, itemdetails: { resourceid: 33, resourcename: 'wheat', status: false } };
+
+    con.query('select * from resource where resourceid =?', [input], (error, rows) => {
+
+        if (rows.length > 0) {
+            output.itemnofoundstatus = true;
+            output.itemdetails = rows[0];
+
+        }
+        resp.send(output);
+
+
+    }
+    );
+
+});
+
+app.get("/addItem", (req, resp) => {
+
+ 
+    let input = { resourceid: req.query.x, resourcename: req.query.y, status: req.query.z };
+    console.log(input);
+    let output = true;
+
+    con.query('insert into resource(resourceid,resourcename,status) values (?,?,?)',
+        [input.resourceid, input.resourcename, input.status],
+        (error, whathappenedtoinsert) => {
+            if (error) {
+
+            }
+
+            else if (whathappenedtoinsert.affectedRows > 0) {
+                output = true;
+
+            }
+
+            resp.send(output);
+
+        }
+    );
+});
+
+
+app.get("/updateitem", (req, resp) => {
+
+    let output = false;
+    let input = {
+        resourceid: req.query.resourceid,
+        resourcename: req.query.resourcename, status: req.query.status
+    };
+
+
+    con.query('update resource set resourcename = ?,status =? where resourceid=?',
+        [input.resourcename, input.status, input.resourceid],
+        (error, whathappenedtoinsert) => {
+            if (error) {
+               
+
+            }
+
+            else if (whathappenedtoinsert.affectedRows > 0)
+                output = true;
+
+
+            resp.send(output);
+
+        }
+
+
+    );
+
+});
+
+app.get("/getAllItems", (req, resp) => {
+
+    con.query('select * from resource', [], (error, rows) => {
+
+
+        resp.send(rows);
+
+    }
+    );
+});
+
+app.listen(900, function () {
+    console.log("server listening at port 900...");
+});
